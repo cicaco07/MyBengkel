11165017bab63ef9f0a2984937fcba404c9efeaf
Aryo <chiefdevica@gmail.com>
2023-05-15 20:42:27 +0700

    add role table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('username');
+            $table->string('alamat');
+            $table->string('no_handphone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/mechanic/servisku.blade.php
+++ resources/views/mechanic/servisku.blade.php
@@ -5,47 +5,94 @@
 @section('main-content')
 <div class="p-4 border-1 rounded-lg mt-14 bg-secondary">
     <div class="text-purple m-4 font-semibold text-2xl tracking-wide">Servisku</div>
-    <div class="mx-12">
-        <div class="flex items-center justify-between mt-4 text-purple bg-primary rounded-t-lg p-4">
-            <div class="">Jenis Motor</div>
-            <div class="">Yamaha Mio Matic 2015</div>
-        </div>
-        <div class="flex items-center justify-between text-purple bg-primary p-4">
-            <div class="">Nomor Polisi</div>
-            <div class="">N 1293 LU</div>
-        </div>
-        <div class="flex items-center justify-between text-purple bg-primary p-4 rounded-b-lg">
-            <div class="">Keluhan</div>
-            <div class="">Sering mogok tengah jalan</div>
-        </div>
-        <div class="my-6">
-            <a href="#" class="mr-4 text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Antrian Selesai</a>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Data Spare Part</a>    
-        </div>
+    <div class="relative overflow-x-auto shadow-md rounded-lg md:mx-20">
+        <table class="w-full text-sm">
+            <tbody class="text-xs md:text-base">
+                <tr class="bg-primary text-purple">
+                    <td class="px-6 py-4">
+                        Aryo Deva Saputra
+                    </td>
+                    <td class="px-6 py-4 text-end">
+                        N 3120 LU
+                    </td>    
+                </tr>
+                <tr class="bg-primary text-purple">
+                    <td class="px-6 py-4">
+                        Bryan Tosin Saputro
+                    </td>
+                    <td class="px-6 py-4 text-end">
+                        L 3192 XS
+                    </td>
+                </tr>
+                <tr class="bg-primary text-purple">
+                    <td class="px-6 py-4">
+                        Desy Ayurianti
+                    </td>
+                    <td class="px-6 py-4 text-end">
+                        AG 2131 C
+                    </td> 
+                </tr>
+            </tbody>
+        </table>
     </div>
-    
+    <div class="text-center md:text-start md:mx-20 my-6">
+        <a href="#" class="mr-4 text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">Antrian Selesai</a>
+        <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">Data Spare Part</a>    
+    </div>
 </div>
 <div class="p-4 border-1 rounded-lg mt-6 bg-secondary">
     <div class="text-purple m-4 font-semibold text-2xl tracking-wide">List Antrian</div>
-    <div class="mx-12">
-        <div class="flex items-center justify-between mt-4 text-purple bg-primary rounded-t-lg p-4">
-            <div class="">N 1293 LU</div>
-            <div class="">Yamaha Mio Matic 2015</div>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">00:10:00</a>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Start</a>
-        </div>
-        <div class="flex items-center justify-between text-purple bg-primary rounded-t-lg p-4">
-            <div class="">N 1293 LU</div>
-            <div class="">Yamaha Mio Matic 2015</div>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">00:10:00</a>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Start</a>
-        </div>
-        <div class="flex items-center justify-between text-purple bg-primary rounded-t-lg p-4">
-            <div class="">N 1293 LU</div>
-            <div class="">Yamaha Mio Matic 2015</div>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">00:10:00</a>
-            <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Start</a>
-        </div>
+    <div class="relative overflow-x-auto shadow-md rounded-lg md:mx-20">
+        <table class="w-full text-sm">
+            <tbody class="text-xs md:text-base">
+                <tr class="bg-primary text-purple">
+                    <td class="px-1 md:px-6 py-4 text-center">
+                        N 3120 LU
+                    </td>
+                    <td class="px-1 md:px-6 py-4 text-center">
+                        Yamaha Matic Mio
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">00:10:00</a>
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="text-primary bg-success hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">Start</a>    
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="text-primary bg-warning hover:bg-yellow-500 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">Finish</a>    
+                    </td>
+                    
+                </tr>
+                <tr class="bg-primary text-purple">
+                    <td class="px-1 md:px-6 py-4 text-center">
+                        N 3120 LU
+                    </td>
+                    <td class="px-1 md:px-6 py-4 text-center">
+                        Yamaha Matic Mio
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">00:10:00</a>
+                    </td>
+                    <td  class="text-center">
+                        <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">Start</a>    
+                    </td>
+                </tr>
+                <tr class="bg-primary text-purple">
+                    <td class="px-1 md:px-6 py-4 text-center">
+                        N 3120 LU
+                    </td>
+                    <td class="px-1 md:px-6 py-4 text-center">
+                        Yamaha Matic Mio
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">00:10:00</a>
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="text-primary bg-dark-purple hover:bg-violet-600 hover:font-semibold focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 text-center">Start</a>    
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
