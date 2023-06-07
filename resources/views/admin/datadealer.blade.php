7725ec6e11b7d95e0c1088d5a172319ec6aebf97
Aryo <chiefdevica@gmail.com>
2023-06-07 13:49:56 +0700

    fix : admin controller

--- resources/views/admin/datadealer.blade.php
+++ resources/views/admin/datadealer.blade.php
@@ -47,7 +47,7 @@
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                     </div>
-                    <input type="text" id="simple-search" name="search" value="{{ $keyword }}" class="bg-primary text-purple text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Search" required>
+                    <input type="text" id="simple-search" autocomplete="off" name="search" value="{{ $keyword }}" class="bg-primary text-purple text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Search" required>
                 </div>
                 
                 <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300">
@@ -58,7 +58,7 @@
             @if(isset($keyword))
                 <div class="text-purple mt-2">Hasil pencarian dari: {{ $keyword }}</div>
             @endif
-        </div>
+        </div>              
     </div>
     <div class="relative overflow-x-auto shadow-md rounded-lg">
         <table class="w-full text-sm text-left text-gray-500">
