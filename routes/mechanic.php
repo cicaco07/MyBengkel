731a547a41b586b74a8635ef014a376fd0f9a3c0
Aryo <chiefdevica@gmail.com>
2023-06-11 15:16:35 +0700

    fix: route

---
+++ routes/mechanic.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\MechanicController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'role:mechanic'])->group(function () {
+    Route::prefix('mechanic')->group(function(){
+        Route::get('/dashboard', [MechanicController::class, 'dashboard'])->name('mechanic.dashboard');
+        Route::get('/profilku', [MechanicController::class, 'profilku']);
+        Route::put('/profilku/update', [MechanicController::class, 'update'])->name('mechanic.update');
+        Route::get('/antrian', [MechanicController::class, 'antrian']);
+        Route::get('/update-service/{id}', [MechanicController::class, 'updateService'])->name('mechanic.updateService');
+        Route::put('/update-status/{id}', [MechanicController::class, 'updateStatus'])->name('mechanic.updateStatus');
+        Route::get('/servisku', [MechanicController::class, 'servisku']);
+    });
+});
