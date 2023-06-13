442d1daeddb632547b1d6fcf4de809b07706083a
daniertg <febriandani00@gmail.com>
2023-06-14 01:39:34 +0700

    Dealer #search data antrian

--- resources/views/dealer/antrian.blade.php
+++ resources/views/dealer/antrian.blade.php
@@ -5,7 +5,31 @@
 @section('main-content')
 
 <div class="p-4 border-1 rounded-lg mt-14 bg-secondary">
-        <div class="text-purple m-4 font-semibold text-2xl tracking-wide">Data Antrian</div>
+    <div class="flex justify-between">
+        <div class="text-purple m-4 font-semibold text-2xl tracking-wide w-1/2">Data Antrian</div>
+        <div class="mt-10 mb-4 w-1/2">
+            <form class="flex items-center" action="{{ route('dealer.search') }}" method="GET">
+                @csrf   
+                <label for="simple-search" class="sr-only">Search</label>
+                <div class="relative w-full">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <input type="text" id="simple-search" autocomplete="off" name="search" class="bg-primary text-purple text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Search" required>
+                </div>
+                
+                <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300">
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <span class="sr-only">Search</span>
+                </button>
+            </form>
+            <div class="text-purple mt-2">
+                @if(!empty($search))
+                    Hasil pencarian dari: {{ $search }}
+                @endif
+            </div>
+        </div>
+    </div>
 <div class="relative overflow-x-auto shadow-md rounded-lg">
     <table class="w-full text-sm text-left text-gray-500">
         <thead class="text-xs text-purple uppercase bg-table-head">
