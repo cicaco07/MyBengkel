be20a054cc840352f6d104eff3a6b24c60fc950a
daniertg <febriandani00@gmail.com>
2023-06-13 13:56:24 +0700

    view login dengan no hp

--- resources/views/auth/otp-verif.blade.php
+++ resources/views/auth/otp-verif.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.header')
+@section('title', 'Login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+
+<div class="flex items-center justify-center h-screen">
+    <div class="w-10/12 md:w-full max-w-sm p-4 bg-secondary rounded-lg shadow sm:p-6 md:p-8">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('OTP Login') }}</div>
-
                 <div class="card-body">
                     @if (session('success'))
                     <div class="alert alert-success" role="alert"> {{session('success')}} 
@@ -17,13 +17,14 @@
                     <div class="alert alert-danger" role="alert"> {{session('error')}} 
                     </div>
                     @endif
-
+                    <h3 class="text-2xl font-medium font-sans text-purple text-center">Login</h3>
+                    <br>
                     <form method="POST" action="{{ route('loginWithOtp') }}">
                         @csrf
                         <input type="hidden" name="user_id" value="{{$user_id}}" />
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <input id="otp" type="text" class="form-control @error('otp') is-invalid @enderror" name="otp" value="{{ old('otp') }}" required autocomplete="otp" autofocus placeholder="Enter OTP">
+                                <input id="otp" type="text" class="bg-transparent border-2 border-purple text-purple text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 @error('otp') is-invalid @enderror" name="otp" value="{{ old('otp') }}" required autocomplete="otp" autofocus placeholder="Enter OTP">
 
                                 @error('otp')
                                     <span class="invalid-feedback" role="alert">
@@ -37,7 +38,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                     {{ __('Login') }}
                                 </button>
                             </div>
--- resources/views/auth/otp.blade.php
+++ resources/views/auth/otp.blade.php
@@ -1,55 +1,19 @@
-@extends('layouts.app')
+@extends('layouts.header')
+@section('title', 'Login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('OTP Login') }}</div>
 
-                <div class="card-body">
-
-                    @if (session('error'))
-                    <div class="alert alert-danger" role="alert"> {{session('error')}} 
-                    </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('generate') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="phone_number" class="col-md-4 col-form-label text-md-end">{{ __('Mobile No') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number"  required autocomplete="phone_number" autofocus placeholder="Enter Your Registered Mobile Number">
-
-                                @error('phone_number')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Generate OTP') }}
-                                </button>
-
-                                {{-- @if (Route::has('login'))
-                                    <a class="btn btn-link" href="{{ route('login') }}">
-                                        {{ __('Login With Username') }}
-                                    </a>
-                                @endif --}}
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="flex items-center justify-center h-screen">     
+    <div class="w-10/12 md:w-full max-w-sm p-4 bg-secondary rounded-lg shadow sm:p-6 md:p-8">
+        <form  action="{{ route('generate') }}" method="POST" class="space-y-6">
+            <h3 class="text-2xl font-medium font-sans text-purple text-center">Login</h3>
+            @csrf
+            <div>
+                <label for="phone_number" class="block mb-2 text-sm font-medium text-purple">Masukan Nomor Telepon</label>
+                <input id="phone_number" type="text" class="bg-transparent border-2 border-purple text-purple text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 @error('phone_number') is-invalid @enderror" name="phone_number"  required autocomplete="phone_number" autofocus placeholder="Masukan Nomor telepon yang terdaftar">
             </div>
-        </div>
+            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Generate OTP</button>
+        </form>
     </div>
 </div>
 @endsection
