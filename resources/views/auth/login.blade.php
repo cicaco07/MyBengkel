16a424de4d41e714fc21d0cd9dfc53b9762c3bae
daniertg <febriandani00@gmail.com>
2023-06-13 11:54:58 +0700

    OTP Login WhatsApp

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                     </div>
                     <label for="remember" class="ml-2 text-sm font-medium text-purple">Remember me</label>
                 </div>
-                <a href="#" class="ml-auto text-sm text-blue-700 hover:underline">Forgot Password?</a>
+                <a href="{{ route('showView') }}" class="ml-auto text-sm text-blue-700 hover:underline">Login dengan No Hp?</a>
             </div>
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
             <div class="text-sm font-medium text-gray-500 text-center">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DealerController;
+use App\Http\Controllers\OtpController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\MechanicController;
@@ -29,6 +30,12 @@ Route::post('/register-validate', [RegisterController::class, 'register'])->name
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('loginForm');
 Route::post('/login-validate', [LoginController::class, 'login'])->name('loginValidate');
+
+Route::get('/otp', [OtpController::class, 'showView'])->name('showView');
+Route::post('/otp/generate', [OtpController::class, 'generate'])->name('generate');
+Route::get('/otp/verification/{user_id}', [OtpController::class, 'verification'])->name('otp.verification');
+Route::post('/otp/login', [OtpController::class, 'loginWithOtp'])->name('loginWithOtp');
+
 
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
