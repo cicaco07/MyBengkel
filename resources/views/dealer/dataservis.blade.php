02df36b3d375ebdf0e45965967b6b0ceed31980e
bryantosin1 <bryantosin1@gmail.com>
2023-06-24 19:53:31 +0700

    fix pagination dealer

--- resources/views/dealer/dataservis.blade.php
+++ resources/views/dealer/dataservis.blade.php
@@ -75,18 +75,10 @@
     
 </div>
 
-<div class="flex flex-col items-center mt-6">
-    <span class="text-sm text-purple">
-        Showing <span class="font-semibold">1</span> to <span class="font-semibold dark:text-white">10</span> of <span class="font-semibold">100</span> Entries
-    </span>
-    <div class="inline-flex mt-2 xs:mt-0">
-        <button class="px-4 py-2 text-sm font-medium text-purple bg-gray-800 rounded-l hover:bg-primary">
-            Prev
-        </button>
-        <button class="px-4 py-2 text-sm font-medium text-purple bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-primary">
-            Next
-        </button>
+    <div class="flex flex-col items-center mt-6">
+        <div class="mt-5 md:mt-0">
+            {{ $services->links('mechanic.pagination') }}
+        </div>
     </div>
-  </div>
 </div>
 @endsection
